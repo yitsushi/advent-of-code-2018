a96e03fa842705d2c2d7c7a4c756e9e1d84b6d4b
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2018-12-14 18:35:43 +0100

    port day-12

--- day12/__init__.py
+++ day12/__init__.py
@@ -1,6 +1,39 @@
 from advent_of_code import BaseSolution
+from .cave import Cave
 
 
 class Solution(BaseSolution):
+    cave: Cave
+
     def setup(self):
-        pass
+        self.cave = Cave()
+
+        (input_file, ) = self.parameters()
+
+        for line in self.read_input(input_file):
+            if "initial state:" in line:
+                self.cave.set_state(line.split(' ')[-1])
+            else:
+                self.cave.add_pattern(line)
+
+    def part1(self) -> int:
+        generations = 20
+        for gen in range(0, generations):
+            self.cave.next()
+
+        return sum(self.cave.pots_with_plant())
+
+    def part2(self) -> int:
+        generations = 50_000_000_000
+        last_value = sum(self.cave.pots_with_plant())
+        diff_history = []
+        for gen in range(0, generations):
+            self.cave.next()
+            current_value = sum(self.cave.pots_with_plant())
+            diff = current_value - last_value
+            last_value = current_value
+
+            diff_history.append(diff)
+            if len(diff_history) > 10:
+                if len(set(diff_history[-10:])) == 1:
+                    return (generations-gen-1) * diff + current_value
--- day12/cave.py
+++ day12/cave.py
@@ -1,16 +1,14 @@
-#!/usr/bin/env python3
-
-import advent_of_code as aoc
 from typing import Dict
 
 
 class Cave:
-    pots = None
-    patterns = None
-    zero_pot_index = None
+    pots: str
+    patterns: Dict[str, str]
+    zero_pot_index: int
 
     def __init__(self):
-        self.patterns: Dict[str, str] = {}
+        self.pots = '.'
+        self.patterns = {}
         self.zero_pot_index = 0
 
     def set_state(self, state: str):
@@ -34,23 +32,5 @@ class Cave:
 
         self.pots = new_pots + '..'
 
-    def number_of_plants(self):
-        return self.pots.count('#')
-
     def pots_with_plant(self):
         return [(i - self.zero_pot_index) for i in range(0, len(self.pots)) if self.pots[i] == '#']
-
-
-cave = Cave()
-
-input_file, generations = aoc.parameters((str, int), ('Input File', 'Generations'), (None, 20))
-for line in aoc.read_input(input_file):
-    if "initial state:" in line:
-        cave.set_state(line.split(' ')[-1])
-    else:
-        cave.add_pattern(line)
-
-for gen in range(0, generations):
-    cave.next()
-
-print(sum(cave.pots_with_plant()))
